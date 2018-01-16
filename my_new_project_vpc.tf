30afdd3ee28e8f949e84c737469b3d1fd4e2559b
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2018-01-16 11:00:29 -0500

    Update my_new_project_vpc.tf

--- my_new_project_vpc.tf
+++ my_new_project_vpc.tf
@@ -14,3 +14,19 @@ resource "google_compute_subnetwork" "subnet_na_e1" {
   project       = "${google_project_services.project.project}"
 }
 
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = "foobar"
+  project = "${google_project_services.project.project}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
+}
