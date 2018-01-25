2475b89126825726142c878f6ab90f50042fdb26
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2018-01-25 11:49:07 -0500

    instance template and group creation

---
+++ my_new_project_template.tf
@@ -0,0 +1,61 @@
+resource "google_compute_instance_template" "instance_template" {
+  name_prefix = "instance-template-"
+  description = "This template is used to create nginx server instances."
+  project = "${google_project_services.project.project}"
+  region      = "northamerica-northeast1"
+
+  tags = ["foo", "bar"]
+
+  labels = {
+    environment = "dev"
+  }
+
+  instance_description = "description assigned to instances"
+  machine_type         = "n1-standard-1"
+  can_ip_forward       = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  // Create a new boot disk from an image
+  disk {
+    source_image = "ubuntu-1604-xenial-v20180122"
+    auto_delete  = true
+    boot         = true
+  }
+
+  // Use an existing disk resource
+#  disk {
+#    source      = "foo_existing_disk"
+#    auto_delete = false
+#    boot        = false
+#  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata {
+    foo = "bar"
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+resource "google_compute_instance_group_manager" "instance_group_manager" {
+  name               = "instance-group-manager"
+  instance_template  = "${google_compute_instance_template.instance_template.self_link}"
+  base_instance_name = "instance-group-manager"
+  zone               = "northamerica-northeast1-a"
+  project = "${google_project_services.project.project}"
+  target_size        = "1"
+}
