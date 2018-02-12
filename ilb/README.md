d87ef2fd9f28488e24dd41b89f8f083dbf9a786d
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2018-02-12 10:59:24 -0500

    typo and ILB

---
+++ ilb/README.md
@@ -0,0 +1,4 @@
+# gcp/ilb
+Terraform script that create an HTTP Internal Load Balancer (with an external IP) linked to a MIG created from the gcp/MIG folder.
+
+Scripts uses a GCP service account and a JSON file with your account token and ENV VARS
--- vpc/custom_vpc.tf
+++ vpc/custom_vpc.tf
@@ -17,13 +17,13 @@ resource "google_compute_subnetwork" "subnet_na_e1" {
   ip_cidr_range = "10.0.0.0/16"
   network       = "${google_compute_network.default.self_link}"
   region        = "northamerica-northeast1"
-  project       = "${google_project_services.project.project}"
+  project = "your_gcp_project"
 }
 
 resource "google_compute_firewall" "default" {
   name    = "test-firewall"
   network = "foobar"
-  project = "${google_project_services.project.project}"
+  project = "your_gcp_project"
 
   allow {
     protocol = "icmp"
