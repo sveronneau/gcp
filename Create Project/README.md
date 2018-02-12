f10132ec1c444a41ef733d4fe4dbc8cd2f7d903e
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2018-02-12 08:40:59 -0500

    cleanup

---
+++ Create Project/README.md
@@ -0,0 +1,4 @@
+# gcp/Create Project
+Shell and Terraform script to create a new GCP Project linked to your billing account.
+
+Scripts uses a GCP service account and a JSON file with your account token and ENV VARS
--- Create Project/gcp_tf_cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-terraform destroy "my_plan"
-#
-gcloud projects delete ${TF_ADMIN}
-#
-gcloud organizations remove-iam-policy-binding ${TF_VAR_org_id} --member serviceAccount:terraform@${TF_ADMIN}.iam.gserviceaccount.com --role roles/resourcemanager.projectCreator
-#
-gcloud organizations remove-iam-policy-binding ${TF_VAR_org_id} --member serviceAccount:terraform@${TF_ADMIN}.iam.gserviceaccount.com  --role roles/billing.user
-#
