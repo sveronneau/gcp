7ca05816ebd21715fab96ac08ca8cedd285b7f8d
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2018-02-14 14:09:47 -0500

    update

---
+++ consul_vars/README.md
@@ -0,0 +1,4 @@
+# gcp/consul_vars
+Terraform and Ansible script to deploy Consul 1.0.6 within a GCP instance.
+
+Requirements: GCP project and Project wide SSH Key
---
+++ consul_vars/consul_with_vars.tf
@@ -0,0 +1,37 @@
+// Configure the Google Cloud provider
+
+provider "google" {
+  credentials = "${var.credentials}"
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+resource "google_compute_instance" "ansible" {
+  count        = "1"
+  project      = "${var.project}"
+  zone         = "${var.zone}"
+  name         = "consul-node-${count.index}"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1604-lts"
+    }
+  }
+
+# This is where we configure the instance with ansible-playbook
+  provisioner "local-exec" {
+    command = "sleep 90; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u your_sshkey_user --private-key your_private_key -i '${google_compute_instance.ansible.network_interface.0.access_config.0.assigned_nat_ip}', master.yml"  }
+
+  network_interface {
+    network       = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
