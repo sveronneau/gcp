3872a44e85a002341892dee3f2f9a33d2ff22d90
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2018-02-28 16:03:35 -0500

    new project

---
+++ packer/apache.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+set -eu -o pipefail
+
+sudo apt-get clean
+sudo apt-get update
+sudo apt-get upgrade -y
+
+PKGS="apache2 htop"
+
+sudo apt-get install $PKGS -y
+sudo ufw allow 'Apache Full'
+
+INT_IP="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"
+EXT_IP="$(curl -s ipinfo.io/ip)"
+
+sleep 30s
+
+cat <<EOF > /var/www/html/index.html
+<html>
+<body>
+<title>Apache Server - $(hostname)</title>
+<img src="https://cloud.google.com/_static/9abbcf9aa7/images/cloud/gcp-logo.svg" alt="Google Cloud" height="51" width="400">
+<h1>Packer baked Apache Server on GCP</h1>
+<p><b>Hostname:</b> $(hostname)</p>
+<p><b>Internal IP:</b> $INT_IP</p>
+<p><b>External IP:</b> $EXT_IP</p>
+<img src="https://blog-en.openalfa.com/iconos/logos/apache_httpd.jpg" alt="Google Cloud" height="100" width="100">
+</body>
+</html>
+EOF
