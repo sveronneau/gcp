274dd09937097095eadcc5e3cb06ddeaf78eceb2
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2018-03-01 08:28:44 -0500

    update

---
+++ MIG/instance_template_and_mig_apache.tf
@@ -0,0 +1,98 @@
+provider "google" {
+  credentials = "${file("gcp_service_account.json")}"
+  project = "your_gcp_project"
+  region  = "northamerica-northeast1"
+}
+
+# data "terraform_remote_state" "foo" {
+#   backend = "gcs"
+#   config {
+#     credentials = "your_json_creds"
+#     bucket      = "your_state_pucket"
+#     prefix      = "terraform/state/MIG"
+#   }
+# }
+
+resource "google_compute_instance_template" "instance_template" {
+  name_prefix = "nginx-"
+  description = "This template is used to create nginx server instances."
+  project = "your_gcp_project"
+  region      = "northamerica-northeast1"
+
+  tags = ["nginx"]
+
+  labels = {
+    environment = "dev"
+  }
+
+  instance_description = "description assigned to instances"
+  machine_type         = "n1-standard-1"
+  can_ip_forward       = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  // Create a new boot disk from an image (Lets use one created by Packer)
+  disk {
+    source_image = "nginx" //image family tag works here
+    auto_delete  = true
+    boot         = true
+  }
+
+  // Use an existing disk resource in RO mode since this is a MIG
+#  disk {
+#    source      = "my_existing_disknginx"
+#    auto_delete = false
+#    boot        = false
+#  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata {
+    foo = "bar"
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+resource "google_compute_instance_group_manager" "instance_group_manager" {
+  name               = "instance-group-manager"
+  instance_template  = "${google_compute_instance_template.instance_template.self_link}"
+  base_instance_name = "instance-group-manager"
+  zone               = "northamerica-northeast1-a"
+  project            = "your_gcp_project"
+  target_size        = "3"
+
+  named_port {
+    name = "http"
+    port = 80
+  }
+
+  named_port {
+    name = "https"
+    port = 443
+  }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "tf-allow-http-https"
+  network = "default"
+  project = "your_gcp_project"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+  target_tags = ["nginx"]
+}
