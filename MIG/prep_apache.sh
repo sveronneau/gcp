828e7e480addfab09e47ef428b9e0a6011d3e6fd
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2018-03-01 08:14:22 -0500

    updates

--- MIG/prep_apache.sh
+++ MIG/prep_apache.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -x
 set -eu -o pipefail
-
+#
 sudo apt-get clean
 sudo apt-get update
 sudo apt-get upgrade -y
-
+#
 PKGS="apache2 htop"
-
+#
 sudo apt-get install $PKGS -y
 sudo ufw allow 'Apache Full'
-
+#
 INT_IP="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"
 EXT_IP="$(curl -s ipinfo.io/ip)"
-
-sleep 15s
-
+#
+sleep 10s
+#
 sudo cat <<EOF > /var/www/html/index.html
 <html>
 <body>
